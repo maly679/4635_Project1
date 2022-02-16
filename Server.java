1069c5837d10e04218916b0e902682c35b1c3fa5
erikszilagyi <35347821+erikszilagyi@users.noreply.github.com>
2022-02-15 21:43:49 -0700

    Add files via upload

---
+++ Server.java
@@ -0,0 +1,96 @@
+/**
+ * Title: COMP4635 Assignment 1
+ * This class is used for the Server that processes requests from clients and interfaces with WordRepository
+ * micro-service.
+ * Usage: java Server [5599] 
+ * @author Mohamed Aly
+ */ 
+
+import java.net.*;
+import java.io.*;
+
+//Class invocation
+public class Server {
+	private static final String USAGE = "Usage: java Server [5599]";
+	private ServerSocket serverSocket;
+	private int counter;
+	private String gameWord = "";
+
+	public Server() throws IOException {
+		this(5599);
+	}
+
+	public Server(int port) throws IOException {
+		serverSocket = new ServerSocket(port);
+	}
+
+	public String setInitialGamePlay(int numWords) {
+		//Setting initial stage of game based on assignment requirements.
+		String initialPlay = "";
+		char [] phraseChar =  gameWord.trim().toCharArray();
+		for (int i = 0; i < phraseChar.length; i ++) {
+			if(Character.isWhitespace(phraseChar[i])) {
+				initialPlay+= " ";
+			} else {
+				initialPlay+= "-";
+			}
+		}
+		return initialPlay+"C" + Integer.toString(counter);	
+
+	}
+
+	public void serve() {
+		//Process the gameplay based on Client.java set parameters for word and factor attempts/assignment requirements.
+		while(true) {
+			try {
+				System.out.println("Listening for incoming requests...");
+				Socket clientSocket = serverSocket.accept();
+				PrintStream out =
+						new PrintStream(clientSocket.getOutputStream());
+				BufferedReader in = new BufferedReader(
+						new InputStreamReader(clientSocket.getInputStream()));
+				String inputLine = in.readLine();
+				int numWords = Integer.parseInt(inputLine.split(" ")[1]); 
+				int factorAttempts = Integer.parseInt(inputLine.split(" ")[2]); 
+				counter = numWords * factorAttempts;
+				DatagramSocket socket = new DatagramSocket();
+				byte[] buf = new byte[256];
+				byte[] inputbuf = new byte[256];
+				buf = Integer.toString(numWords).getBytes();
+				InetAddress address = InetAddress.getByName("localhost");
+				DatagramPacket packet = new DatagramPacket(buf, buf.length, address, 5599);
+				socket.send(packet);
+				DatagramPacket udpReplyPacket = 
+						new DatagramPacket(inputbuf, inputbuf.length, address, 5599);
+				socket.receive(udpReplyPacket);
+				gameWord = new String(udpReplyPacket.getData());
+				String initialGamePlay = setInitialGamePlay(numWords);
+				out.println(initialGamePlay);
+				clientSocket.close();
+			} catch (SocketException e) {
+				System.out.println(e.getMessage());
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}		
+	}
+
+	public static void main(String[] args) throws IOException {
+		if (args.length != 1) {
+			System.err.println(USAGE);
+			System.exit(1);
+		}
+
+		int port = 0;
+		Server server = null;
+		try {
+			port = Integer.parseInt(args[0]);
+			server = new Server(port);
+		} catch (IOException e) {
+			System.out.println("Exception caught when trying to listen on port "
+					+ port + " or listening for a connection");
+			System.out.println(e.getMessage());
+		}
+		server.serve();
+	}
+}
---
+++ WordRepository.java
@@ -0,0 +1,110 @@
+/**
+ * Title: COMP4635 Assignment 1
+ * This class is used as a word repository that is to store and process words and word changes during the game.
+ * Usage: java WordRepository [5599] 
+ * @author Mohamed Aly
+ */ 
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.util.ArrayList;
+
+//Class invocation
+public class WordRepository {
+	private static final String USAGE = "Usage: java WordRepository [5599]";
+	DatagramSocket socket;
+	static ArrayList<String> words = new ArrayList<>();
+	public WordRepository() throws IOException {
+		this(5599);
+	}
+
+	public WordRepository(int port) throws IOException {
+		socket = new DatagramSocket(port);
+	}
+
+	public String generateRandomWord(int wordCount) {
+	//Generate a random word based on server's request.
+
+		String generatedWord = "";
+		for (int i = 0; i < wordCount; i++) 
+		{
+			// generating the index using Math.random()
+			int index = (int) ((Math.random() * (words.size() - 1)) + 1);
+
+			generatedWord = generatedWord + words.get(index)  + " " ;
+		}
+		return generatedWord;
+
+	}
+
+	public void serve() {
+		//Process UDP based communication, implementing required logic.
+		while(true) {
+			try {
+				System.out.println("Listening for incoming requests...");
+				byte[] inputbuf = new byte[256];
+				byte[] outputbuf = new byte[256];
+
+				// receive request
+				DatagramPacket udpRequestPacket = new DatagramPacket(inputbuf, inputbuf.length);
+				socket.receive(udpRequestPacket);	              
+				outputbuf =  generateRandomWord(Integer.parseInt(new String(udpRequestPacket.getData()).trim())).getBytes();
+				InetAddress address = udpRequestPacket.getAddress();
+				int port = udpRequestPacket.getPort();
+
+				DatagramPacket udpReplyPacket = 
+						new DatagramPacket(outputbuf, outputbuf.length, address, port);
+
+				socket.send(udpReplyPacket);
+
+			} catch (SocketException e) {
+				System.out.println(e.getMessage());
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}	
+	}
+
+
+	public static void main(String[] args) throws IOException {
+		if (args.length != 1) {
+			System.err.println(USAGE);
+			System.exit(1);
+		}
+		
+		//location set for words text file
+		BufferedReader bufReader = new BufferedReader(new FileReader("../words.txt")); 
+		String line = bufReader.readLine(); 
+		while (line != null) { words.add(line); 
+		line = bufReader.readLine();
+		}
+		bufReader.close();
+
+
+		int port = 0;
+		WordRepository server = null;
+		try {
+			port = Integer.parseInt(args[0]);
+			server = new WordRepository(port);
+
+		} catch (NumberFormatException e) {
+			System.err.println("Invalid port number: " + port + ".");
+			System.exit(1);
+		} catch (IOException e) {
+			System.out.println("Exception caught when trying to listen on port "
+					+ port);
+			System.out.println(e.getMessage());
+		}
+
+
+		server.serve();
+		server.socket.close();
+
+	}
+
+}
